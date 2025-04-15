1d6ec41733236abd0e5ffa679ecad3086ab921eb
luca <luca.barsottini@motusml.com>
2025-04-16 00:06:03 +0200

    added the bucket for terraform state

--- terraform_main/main.tf
+++ terraform_main/main.tf
@@ -1,3 +1,21 @@
+
+# Extremely important in order to keep all the .tfstate of terraform and be able to use Pipeline
+# to destroy or create
+terraform {
+  # Configure the backend to use GCS for storing the Terraform state.
+  backend "gcs" {
+    # Name of the GCS bucket where the state will be stored
+    bucket = "consummate-fold-456420-b6_state_bucket_terraform" # This is your GCS bucket name
+
+    # Prefix (optional) for organizing the state files inside the bucket
+    # This can be useful if you have multiple environments, e.g., dev, staging, prod
+    prefix = "terraform/state" # This specifies the folder structure within the bucket
+  }
+}
+
+# The rest of your Terraform configuration would go here (resources, modules, etc.)
+
+
 provider "google" {
   project     = var.project_id
   region      = var.region
